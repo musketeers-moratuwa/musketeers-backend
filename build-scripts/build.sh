5ef2d3a795d0cd26b922d5fb4e90a973496679a1
Manupa Wick <manupanewsletter@gmail.com>
2025-03-08 01:03:40 +0530

    Seperating the Build Scripts

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -7,7 +7,7 @@ MANIFEST_FILE="MANIFEST.MF"
 
 # Step 1: Compile the Java file
 echo "Compiling $MAIN_CLASS.java..."
-javac "$MAIN_CLASS.java"
+javac ../"$MAIN_CLASS.java"
 
 # Check if compilation was successful
 if [ $? -ne 0 ]; then
@@ -17,15 +17,15 @@ fi
 
 # Step 2: Create the Manifest file
 echo "Creating manifest file..."
-echo -e "Main-Class: $MAIN_CLASS\n" > $MANIFEST_FILE
+echo -e "Main-Class: $MAIN_CLASS\n" > ../$MANIFEST_FILE
 
 # Step 3: Create the JAR file
 echo "Packaging into JAR..."
-jar cfm "$JAR_NAME" "$MANIFEST_FILE" "$MAIN_CLASS.class"
+jar cfm ../"$JAR_NAME" ../"$MANIFEST_FILE" ../"$MAIN_CLASS.class"
 
 # Step 4: Clean up (optional)
 # rm "$MANIFEST_FILE"
 
 # Step 5: Run the JAR file
 echo "Running the JAR file..."
-java -jar "$JAR_NAME"
+java -jar ../"$JAR_NAME"
